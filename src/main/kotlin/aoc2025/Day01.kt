15eadb74948281ba923f4fb8398b56914eac6a6d
Thomas Hoffmann <thomas.hoffmann2@rewe-group.com>
2025-12-01 07:47:15 +0100

    day1 2025

--- src/main/kotlin/aoc2025/Day01.kt
+++ src/main/kotlin/aoc2025/Day01.kt
@@ -1,23 +1,53 @@
 package aoc2025
 
+import modulo
 import readInput
 
 object Day01 {
-	fun part1(input: List<String>): Int {
-		return 0
-	}
 
-	fun part2(input: List<String>): Int {
-		return 0
-	}
+    private fun dial(input: List<String>): Pair<Int, Int> {
+        var currentPosition = 50
+        var countZeroPosition = 0
+        var countPasses = 0
+        val instructions = input.map { line -> line.first() to line.drop(1).toInt() }
+        instructions.forEach { (direction, distance) ->
+            val (newPosition, passes) = when (direction) {
+                'R' -> {
+                    val newPosition = (currentPosition + distance) modulo 100
+                    val passes = ((currentPosition + distance) / 100.0).toInt()
+                    newPosition to passes
+                }
+
+                'L' -> {
+                    val newPosition = (currentPosition - distance) modulo 100
+                    val passes = ((currentPosition - distance) / -100.0 + (if (currentPosition != 0) 1 else 0)).toInt()
+                    newPosition to passes
+                }
+
+                else -> error("Unknown direction: $direction")
+            }
+            currentPosition = if (newPosition == 100) 0 else newPosition
+            if (currentPosition == 0) countZeroPosition++
+            countPasses += passes
+        }
+        return countZeroPosition to countPasses
+    }
+
+    fun part1(input: List<String>): Int {
+        return dial(input).first
+    }
+
+    fun part2(input: List<String>): Int {
+        return dial(input).second
+    }
 }
 
 fun main() {
-	val testInput = readInput("Day01_test", 2025)
-	check(Day01.part1(testInput) == 0)
-	check(Day01.part2(testInput) == 0)
+    val testInput = readInput("Day01_test", 2025)
+    check(Day01.part1(testInput) == 3)
+    check(Day01.part2(testInput) == 6)
 
-	val input = readInput("Day01", 2025)
-	println(Day01.part1(input))
-	println(Day01.part2(input))
+    val input = readInput("Day01", 2025)
+    println(Day01.part1(input))
+    println(Day01.part2(input))
 }
