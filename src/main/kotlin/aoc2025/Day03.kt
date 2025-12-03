35d1ff4d748b75372f0b3171278d43ef664f6da1
Thomas Hoffmann <thomas.hoffmann2@rewe-group.com>
2025-12-03 07:36:31 +0100

    day03

--- src/main/kotlin/aoc2025/Day03.kt
+++ src/main/kotlin/aoc2025/Day03.kt
@@ -3,21 +3,48 @@ package aoc2025
 import readInput
 
 object Day03 {
-	fun part1(input: List<String>): Int {
-		return 0
-	}
 
-	fun part2(input: List<String>): Int {
-		return 0
-	}
+    private data class ValueWithIndex(val index: Int, val value: Int)
+
+    private fun String.findLargesBatteryWithIndex(maxIndex: Int): ValueWithIndex {
+        var foundDigit = 0
+        var foundIndex = 0
+        for (index in 0..<maxIndex) {
+            val digit = this[index].digitToInt()
+            if (digit > foundDigit) {
+                foundDigit = digit
+                foundIndex = index
+            }
+        }
+        return ValueWithIndex(index = foundIndex, value = foundDigit)
+    }
+
+    private fun String.findLargesJoltage(batteries: Int): Long {
+        val digits = mutableListOf<Int>()
+        var remainingBanks = this
+        for (battery in 0..<batteries) {
+            val (index, digit) = remainingBanks.findLargesBatteryWithIndex(remainingBanks.length - (batteries - battery - 1))
+            digits.add(digit)
+            remainingBanks = remainingBanks.drop(index + 1)
+        }
+        return digits.joinToString("").toLong()
+    }
+
+    fun part1(input: List<String>): Int {
+        return input.sumOf { it.findLargesJoltage(2) }.toInt()
+    }
+
+    fun part2(input: List<String>): Long {
+        return input.sumOf { it.findLargesJoltage(12) }
+    }
 }
 
 fun main() {
-	val testInput = readInput("Day03_test", 2025)
-	check(Day03.part1(testInput) == 0)
-	check(Day03.part2(testInput) == 0)
+    val testInput = readInput("Day03_test", 2025)
+    check(Day03.part1(testInput) == 357)
+    check(Day03.part2(testInput) == 3121910778619L)
 
-	val input = readInput("Day03", 2025)
-	println(Day03.part1(input))
-	println(Day03.part2(input))
+    val input = readInput("Day03", 2025)
+    println(Day03.part1(input))
+    println(Day03.part2(input))
 }
